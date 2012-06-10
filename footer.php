2e8efbef2f238ce7c2801b589346e2c2d44eea21
Adam Schweigert <aschweigert@gmail.com>
2012-06-09 20:57:24 -0400

    add footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<hr>
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <script src="<?php bloginfo('template_directory'); ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
